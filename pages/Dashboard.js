2770fb7272acfe6ba98e03cbdec94fe2a6fbf83f
SeniorDev1207 <rabbitdev1207@gmail.com>
2022-01-24 01:19:33 +0100

    feat: add simple wallet connection button

---
+++ pages/Dashboard.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Dashboard = ({address}) => {
+    return (
+        <div>{address}</div>
+    )
+
+}
+
+export default Dashboard;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
 import '../styles/globals.css'
+import { ThirdwebWeb3Provider } from '@3rdweb/hooks'
+
+const supportedChainIds = [4]
+const connector = {
+  injected: {}
+}
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebWeb3Provider
+      supportedChainIds={supportedChainIds}
+      connectors={connector}
+    >
+      <Component {...pageProps} />
+    </ThirdwebWeb3Provider>
+  )
 }
 
 export default MyApp
