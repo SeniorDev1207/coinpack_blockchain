94a3861107e68af9a700331814ef3dd06b5d932a
SeniorDev1207 <rabbitdev1207@gmail.com>
2022-01-26 23:31:48 +0100

    feat: add basic portfolio component

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -1,10 +1,30 @@
 import React from "react"
+import Header from "../components/Header"
+import styled from 'styled-components'
+import Main from "../components/Main"
 
 const Dashboard = ({address}) => {
     return (
-        <div>{address}</div>
+        <Wrapper>
+            <MainContainer>
+                <Header />
+                <Main />
+            </MainContainer>
+        </Wrapper>
     )
 
 }
 
 export default Dashboard;
+
+const Wrapper = styled.div`
+display: flex;
+height: 100vh;
+width: 100vw;
+background-color: #0a0b0d;
+color: #fff;
+`
+
+const MainContainer = styled.div`
+flex: 1;
+`
