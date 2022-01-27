30b0db4f2a9696595ea3002d153571537abd87d5
SeniorDev1207 <rabbitdev1207@gmail.com>
2022-01-27 13:29:00 +0100

    feat: add wallet status

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,19 @@
 import React from "react"
 import styled from 'styled-components'
 
-const Header = () => {
+const Header = ({walletAddress, connected}) => {
     return (
         <Wrapper>
             <Title>Assets</Title>
             <ButtonsContainer>
+                <WalletLink>
+                    <WalletLinkTitle>
+                        Wallet connected
+                    </WalletLinkTitle>
+                    <WalletAddress>
+                        {walletAddress.slice(0, 7)}...{walletAddress.slice(35)}
+                    </WalletAddress>
+                </WalletLink>
                 <Button style={{backgroundColor: '#3773f5', color: '#000'}}>
                     Buy / Sell
                 </Button>
@@ -48,4 +56,24 @@ const Button = styled.div`
     }
 
 `
-
+const WalletLink = styled.div`
+    font-size: 0.8rem;
+    border: 1px solid #282b2f;
+    border-radius: 50rem;
+    font-size: 1.2rem;
+    margin-right: 1rem;
+    padding: 0rem 1rem;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+`
+const WalletLinkTitle = styled.div`
+    font-size: 1.1rem;
+    margin-bottom: 0.3rem;
+    color: #27ad75;
+    font-weight: 600;
+`
+const WalletAddress = styled.div`
+    font-size: 0.8rem;
+`
